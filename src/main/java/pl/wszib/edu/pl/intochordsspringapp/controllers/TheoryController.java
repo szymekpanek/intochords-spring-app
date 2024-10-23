ee6039fcfbfa2da7361d82f29d3d49d5149b59c0
szymekpanek20 <szymon.panek@partnerzyjutra.pl>
2024-10-23 12:45:59 +0200

    added theory page

---
+++ src/main/java/pl/wszib/edu/pl/intochordsspringapp/controllers/TheoryController.java
@@ -0,0 +1,38 @@
+package pl.wszib.edu.pl.intochordsspringapp.controllers;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/theory")
+public class TheoryController {
+
+    @GetMapping
+    public String theoryPage() {
+        return "theory";
+    }
+
+    @GetMapping("/download/{filename:.+}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String filename) {
+        Resource resource = new ClassPathResource("static/pdf/" + filename);
+
+        if (!resource.exists()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + filename + "\"");
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .body(resource);
+    }
+}
